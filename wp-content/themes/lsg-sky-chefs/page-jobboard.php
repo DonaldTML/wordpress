3c827348c3b12b7c00973ca3d1946dfe647fa96f
DonaldTML <donald@martianlogic.com>
2023-04-05 13:24:51 +1000

    Update page-jobboard.php

--- wp-content/themes/lsg-sky-chefs/page-jobboard.php
+++ wp-content/themes/lsg-sky-chefs/page-jobboard.php
@@ -4,7 +4,7 @@ get_post();
 // Template Name: Front Page
 ?>
 <div class="banner-container">
-	
+	<img src="http://localhost/wordpress/wp-content/themes/lsg-sky-chefs/images/banner_chef.jpg"/>
 	<div class="banner-image"></div>
     <div class="banner">
     </div>
